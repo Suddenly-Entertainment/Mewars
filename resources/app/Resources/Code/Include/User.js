e9dc3dc68586c49db39ee650916a18e9a9e96872
Ryex <Ryexander@gmail.com>
2013-02-18 21:59:05 -0700

    More work on XML parser
    - Introdused Sizers
    - did some renameing
    - restructured so the code is more dynamic and easier to extend
    - made it so the Rails production envierment determins the resource url
    used in the erb template in other words running the server localy will
    let the resource url be local. meaning the entier game can be tested
    local if your computer is set up right.

--- resources/app/Resources/Code/Include/User.js
+++ resources/app/Resources/Code/Include/User.js
@@ -1,22 +1,20 @@
 Crafty.c("NetworkUser", {
-	init: function () {
-		this.requires("Network");
-	},
+    init: function() {
+        this.requires("Network");
+    },
 
-	GetUserData: function () {
-		this.Send("GameLoadUser", "Game", "load_user", {});
-	},
-	
-	Login: function (login, password) {
-		this.Send("UsersLogin", 
-			{
-				user_login: {
-                    login: login,
-                    password: password,
-                }
-			}
-		);
-	}
+    GetUserData: function() {
+        this.Send("GameLoadUser", "Game", "load_user", {});
+    },
+
+    Login: function(login, password) {
+        this.Send("UsersLogin", {
+            user_login: {
+                login: login,
+                password: password
+            }
+        });
+    }
 });
 
 
@@ -28,31 +26,31 @@ $MEW.User = {
     is_admin: null,
     is_logged: null,
     is_mod: null,
-    
+
     load_user: function(cb) {
-    	$MEW.User.cb = cb;
-    	$MEW.Network.requires("NetworkUser");
-    	$MEW.Network.unbind("GameLoadUser", $MEW.User.onLoadUser);
-    	$MEW.Network.bind("GameLoadUser", $MEW.User.onLoadUser);
+        $MEW.User.cb = cb;
+        $MEW.Network.requires("NetworkUser");
+        $MEW.Network.unbind("GameLoadUser", $MEW.User.onLoadUser);
+        $MEW.Network.bind("GameLoadUser", $MEW.User.onLoadUser);
         $MEW.Network.GetUserData();
     },
-    
-    onLoadUser: function (result) {
-    	$MEW.User.data = result;
-    	$MEW.User.name = result.context.name;
-    	$MEW.User.avatar_href = result.context.avatar.href;
-    	$MEW.User.is_guest = result.context.is_guest;
-    	$MEW.User.is_admin = result.context.is_admin;
-    	$MEW.User.is_logged = result.context.is_logged;
-    	$MEW.User.is_mod = result.context.is_mod;
-        if (!(typeof($MEW.User.cb)==='undefined')) $MEW.User.cb();      
+
+    onLoadUser: function(result) {
+        $MEW.User.data = result;
+        $MEW.User.name = result.context.name;
+        $MEW.User.avatar_href = result.context.avatar.href;
+        $MEW.User.is_guest = result.context.is_guest;
+        $MEW.User.is_admin = result.context.is_admin;
+        $MEW.User.is_logged = result.context.is_logged;
+        $MEW.User.is_mod = result.context.is_mod;
+        if(!(typeof($MEW.User.cb) === 'undefined')) $MEW.User.cb();
     },
-    
-    Login: function (username, password, cb) {
-    	$MEW.Network.requires("NetworkUser");
-    	$MEW.Network.unbind("UsersLogin", cb);
-    	$MEW.Network.bind("UsersLogin", cb);
-    	$MEW.Network.Login(username, password);
+
+    Login: function(username, password, cb) {
+        $MEW.Network.requires("NetworkUser");
+        $MEW.Network.unbind("UsersLogin", cb);
+        $MEW.Network.bind("UsersLogin", cb);
+        $MEW.Network.Login(username, password);
     }
-    
+
 };
--- resources/app/Resources/Code/Include/XML.js
+++ resources/app/Resources/Code/Include/XML.js
@@ -1,137 +1,234 @@
-function XMLInterfaceNode (xml) {
-    this.minWidth = 0;
-    this.minHeight = 0;
-    this.x = 0;
-    this.y = 0;
-    this.z = 0;
-    this.portionWidth = 0;
-    this.portionHeight = 0;
-    this.width = 0;
-    this.height = 0;
-    this.xml = xml;
-    this.paddingTop = 0;
-    this.paddingBottom = 0;
-    this.paddingLeft = 0;
-    this.paddingRight = 0;
+$XMLAttrParsers = {
+    component: function (that, ent, attr, base){
+        ent.require(attr.nodeValue);
+    },
+    name: function (that, ent, attr, base) {
+        ent.setName(attr.nodeValue);
+        that.name = attr.nodeValue;
+    },
+    x: function (that, ent, attr, base) {
+        var val = atter.nodeValue;
+        if(!isNaN(val)) {
+            val = parseFloat(val);
+        }
+        ent.x = val + base.basex || base.basex;
+    },
+    y: function (that, ent, attr, base) {
+        var val = atter.nodeValue;
+        if(!isNaN(val)) {
+            val = parseFloat(val);
+        }
+        ent.y = val + base.basey || base.basey;
+    },
+    z: function (that, ent, attr, base) {
+        var val = atter.nodeValue;
+        if(!isNaN(val)) {
+            val = parseFloat(val);
+        }
+        ent.z = val + base.basez || base.basez;
+    }
+};
+
+$XMLNodeParsers = {
+    e: function (that, ent, entities, nodes, node, base){
+        var inter = new XMLEntityNode(node);
+        nodes.push(inter);
+    },
+    sizer: function (that, ent, entities, nodes, node, base) {
+        var sizer = new XMLSizerNode(node);
+        nodes.push(sizer);
+    }
+};
+
+$XMLNodeDelayParsers = {
+    e: function (that, ent, entities, node, base){
+        entities.push(that.parseNode(node, base.basex, base.basey, base.basez, base.width, base.height));
+    },
+    sizer: function (that, ent, entities, nodes, node, base) {
+        var entity = that.parseNode(node, base.basex, base.basey, base.basez, base.width, base.height);
+        entities.push(entity);
+        node.layout(entity);
+    },
+    init: function (that, ent, entities, node, base) {
+        ent[node.childNodes[0].nodeValue]();
+    },
+    attr: function (that, ent, entities, node, base) {
+        _(node.childNodes).each( function( attr, index, attributes) {
+            var val = attr.childNodes[0].nodeValue;
+            if(!isNaN(val)) {
+                val = parseFloat(val);
+            }
+            ent.attr(attr.nodeName.toLowerCase(), val);
+        });
+    },
+    html: function (that, ent, entities, node, base) {
+        ent.replace(node.childNodes[0].nodeValue);
+    },
+    css: function (that, ent, entities, node, base) {
+        ent.css(JSON.parse(node.childNodes[0].nodeValue));
+    },
+    text: function (that, ent, entities, node, base) {
+        _(node.childNodes).each( function( node, index, nodes) {
+            if (node.nodeName.toLowerCase() === 'value') {
+                var text = node.childNodes[0].nodeValue;
+                ent.text(text);
+            } else if (node.nodeName.toLowerCase() === 'textcolor') {
+                var color = JSON.parse(node.childNodes[0].nodeValue);
+                ent.textColor(color[0], color[1]);
+            } else if (node.nodeName.toLowerCase() === 'textfont') {
+                var font = JSON.parse(node.childNodes[0].nodeValue);
+                ent.textFont(font);
+            }
+        });
+    }
+};
+
+
+
+function XMLSizerNode (xml) {
+    this.name    = '';
+    this.type    = '';
+    this.x       = 0;
+    this.y       = 0;
+    this.z       = 0;
+    this.portion = 0;
+    this.width   = 0;
+    this.height  = 0;
+    this.xml     = xml;
+    this.ptop    = 0;
+    this.pbottom = 0;
+    this.pleft   = 0;
+    this.pright  = 0;
+    this.align   = '';
+    this.expand  = true;
+    this.rows    = 0;
+    this.cols    = 0;
+    this.vgap    = 0;
+    this.hgap    = 0;
     _(xml.attributes).each( function( atter, index, attributes ) {
         var val = atter.nodeValue;
         if(!isNaN(val)) {
             val = parseFloat(val);
         }
-        this[atter.nodeName.toLowerCase()] = val;  
+        this[atter.nodeName.toLowerCase()] = val;
+    });
+    this.layout = function (ent) {
+        if (this.type === 'verticle') {
+            _(ent.entities).each( function( ent, index, entities ) {
+                
+            });
+
+        } else if (this.type === 'horizontal') {
+
+        } else if (this.type === 'grid') {
+
+        }
+        // var x = 0, y = 0;
+        // figure out the size of each node and parse it
+        // var lessminWidth     = width - minWidth,
+        //     lessminHeight    = height - minHeight,
+        //     widthportion     = 0,
+        //     heightportion    = 0;
+
+        // if (lessminWidth > 0) {
+        //     widthportion = Math.round(lessminWidth / widthportions);
+        // }
+        // if (lessminHeight > 0) {
+        //     heightportion = Math.round(lessminHeight / heightportions);
+        // }
+        // var subwidth = widthportion * inter.portionWidth + inter.minWidth,
+        //     subheight = heightportion * inter.portionHeight + inter.minHeight;
+        // x += subwidth + inter.paddingLeft;
+        // y += subheight + inter.paddingTop;
+        // x += inter.paddingRight;
+        // y += inter.paddingBottom;
+    };
+}
+
+
+function XMLEntityNode (xml) {
+    this.name      = '';
+    this.component = '';
+    this.minwidth  = 0;
+    this.minheight = 0;
+    this.x         = 0;
+    this.y         = 0;
+    this.z         = 0;
+    this.portion   = 0;
+    this.width     = 0;
+    this.height    = 0;
+    this.xml       = xml;
+    this.ptop      = 0;
+    this.pbottom   = 0;
+    this.pleft     = 0;
+    this.pright    = 0;
+    this.align     = '';
+    this.expand    = true;
+    _(xml.attributes).each( function( atter, index, attributes ) {
+        var val = atter.nodeValue;
+        if(!isNaN(val)) {
+            val = parseFloat(val);
+        }
+        this[atter.nodeName.toLowerCase()] = val;
     });
 }
 
 function XMLInterface (xml) {
-    this.name = "";
-    this.delayNodes = ['css', 'html', 'init', 'attr'];
-    this.ent = null;
-    this.entities = [];
-    this.xml = xml;
+    this.name       = "";
+    this.delayNodes = ['css', 'html', 'init', 'attr', 'text', 'sizer'];
+    this.ent        = null;
+    this.entities   = [];
+    this.xml        = xml;
 
-    this.parseInterface = function (xml, basex, basey, basez, width, height) {
+    this.parseNode = function (xml, basex, basey, basez, width, height) {
 
-        var entities = [],
-            delay_nodes = [],
-            parse_interfaces = [],
-            minWidth = 0,
-            minHeight = 0,
-            widthportions = 0,
-            heightportions = 0,
-            children = xml.childNodes,
-            attributes = xml.attributes,
-            ent = Crafty.e("2D");
+        var entities     = [],
+            delay_nodes  = [],
+            nodes        = [],
+            children     = xml.childNodes,
+            attributes   = xml.attributes,
+            ent          = Crafty.e("2D");
 
         // get child nodes and figureout how to parse them
+        var base = {basex: basex, basey: basey, basez: basez, width: width, height: height};
+        var that = this;
+
         _(children).each( function( node, index, children ) {
-            if (node.nodeName === 'e') {
-                var inter = new XMLInterfaceNode(node);
-                minWidth += inter.minWidth + inter.paddingRight + inter.paddingLeft;
-                minHeight += inter.minHeight + inter.paddingTop + inter.paddingBottom;
-                widthportions += inter.portionWidth;
-                heightportions += inter.portionHeight;
-                parse_interfaces.push(inter);
-            } else if (_(this.delayNodes).contains(node.nodeName)) {
+            if ($XMLNodeParsers.hasOwnProperty(node.nodeName.toLowerCase())) {
+                $XMLNodeParsers[node.nodeName.toLowerCase()](that, ent, entities, nodes, node, base);
+            }
+            if ($XMLNodeDelayParsers.hasOwnProperty(node.nodeName.toLowerCase())) {
                 delay_nodes.push(node);
             }
         });
 
         // parse attributes
-        _(attributes).each( function( atter, index, attributes ) {
-            if (atter.nodeName === 'class') {
-                ent.require(atter.nodeValue);
-            } else if (atter.nodeName === 'name') {
-                ent.setName(atter.nodeValue);
-                this.name = atter.nodeValue;
+        _(attributes).each( function( attr, index, attributes ) {
+            if ($XMLAttrParsers.hasOwnProperty(node.nodeName.toLowerCase())){
+                $XMLAttrParsers[node.nodeName.toLowerCase()](that, ent, attr, base);
             } else {
                 var val = atter.nodeValue;
                 if(!isNaN(val)) {
                     val = parseFloat(val);
                 }
-                ent[atter.nodeName.toLowerCase()] = val;
+                ent.attr(attr.nodeName.toLowerCase(), val);
             }
-        });
-
-        // set position relative to parent node
-        ent.x = ent.x + basex || basex;
-        ent.y = ent.y + basey || basey;
-        ent.z = ent.z + basez || basez;
-
-        // figure out the size of each node and parse it
-        var lessminWidth = width - minWidth,
-            lessminHeight = height - minHeight,
-            widthportion = 0,
-            heightportion = 0;
-
-        if (lessminWidth > 0) {
-            widthportion = Math.round(lessminWidth / widthportions);
-        }
-        if (lessminHeight > 0) {
-            heightportion = Math.round(lessminHeight / heightportions);
-        }
-
-        var x = 0, y = 0;
-        _(parse_interfaces).each( function( inter, index, parse_interfaces ) {
-            var subwidth = widthportion * inter.portionWidth + inter.minWidth,
-                subheight = heightportion * inter.portionHeight + inter.minHeight;
-            x += subwidth + inter.paddingLeft;
-            y += subheight + inter.paddingTop;
-            entities.push(this.parseInterface(inter.node, basex + x, basey + y, basez, subwidth, subheight));
-            x += inter.paddingRight;
-            y += inter.paddingBottom;
         });
 
         // set attributes in sub nodes
         _(delay_nodes).each( function( node, index, delay_nodes ) {
             try {
-                if (node.nodeName === 'html') {
-                    ent.replace(node.childNodes[0].nodeValue);
-                } else if (node.nodeName === 'css') {
-                    ent.css(JSON.parse(node.childNodes[0].nodeValue));
-                } else if (node.nodeName === 'init') {
-                    ent[node.childNodes[0].nodeValue]();
-                } else if (node.nodeName === 'attr') {
-                    _(node.childNodes).each( function( attr, index, attributes) {
-                        var val = attr.childNodes[0].nodeValue;
-                        if(!isNaN(val)) {
-                            val = parseFloat(val);
-                        }
-                        ent[attr.nodeName.toLowerCase()] = val;
-                    });
-                }
+                $XMLNodeDelayParsers[node.nodeName.toLowerCase()](that, ent, entities, node, base);
             } catch (e) {
-                errortxt = "Error Phrasing Interface " + this.name + "on node: " + ent.name + ":" + node.nodeName;
+                errortxt = "Error Phrasing Interface " + this.name + "on node: " + ent.name + ":" + node.nodeName.toLowerCase();
                 console.log(errortxt, e.message, e);
             }
         });
-
-        
 
         _(entities).each( function( e, index, entities ) {
             ent.attach(e);
         });
 
-        this.entities.push(ent);
         return ent;
     };
 
@@ -141,7 +238,7 @@ function XMLInterface (xml) {
             width = base.w,
             heigt = base.h,
             basez = base.z;
-        var inter = this.parseInterface(this.xml, basex, basey, basez, width, height);
+        var inter = this.parseNode(this.xml, basex, basey, basez, width, height);
         base.attach(inter);
     };
     
@@ -153,7 +250,7 @@ function XMLInterfacePraser(xml) {
     this.interfaces = [];
     this.map = [];
 
-    this.parseInterfaces = function (xml) { 
+    this.parseInterfaces = function (xml) {
         var interfaces = xml.getElementsByTagName('interface');
 
         _(interfaces).each( function( interfaceXML, index, interfaces ) {
