0c1a56dca95ae5685b7e6f9b340d28f396cbf100
capt-glorypants <captglorypants@hotmail.com>
2013-03-17 18:38:32 +0100

    test

--- resources/app/Resources/Code/Include/PonymakerTest.js
+++ resources/app/Resources/Code/Include/PonymakerTest.js
@@ -1,13 +1,39 @@
-Crafty.scene("PonymakerTest", function() {
-    Crafty.viewport.x = 0;
-    Crafty.viewport.y = 0;
-
-    if ($MEW.compositing == 0) {
-        $MEW.player = Crafty.e("Player").makePlayer(200, 200, "0|0|0|0|0|11|0|0|0|0|0|0|0");
-    } else {
-        $MEW.player = Crafty.e("CanvasPlayer").makePlayer(200, 200, "0|0|0|0|0|11|0|0|0|0|0|0|0");
-    }
-    $MEW.player.armorColorOld = 0;
-    $MEW.player.hatColorOld = 0;
-    $MEW.interface = Crafty.e("Interface").makeInterface(0, 500, 1);
-});
+Crafty.scene("PonymakerTest", function() {
+
+
+    Crafty.viewport.x = 0;
+
+
+    Crafty.viewport.y = 0;
+
+
+
+
+
+    if ($MEW.compositing == 0) {//messing around with variables to understand how things work
+
+
+        $MEW.player = Crafty.e("Player").makePlayer(400, 400, "0|0|0|0|0|11|0|0|0|0|0|0|0");
+
+
+    } else {
+
+
+        $MEW.player = Crafty.e("CanvasPlayer").makePlayer(400, 400, "0|0|0|0|0|11|0|0|0|0|0|0|0");
+
+
+    }
+
+
+    $MEW.player.armorColorOld = 0;
+
+
+    $MEW.player.hatColorOld = 0;
+
+
+    $MEW.interface = Crafty.e("Interface").makeInterface(0, 500, 1);
+
+
+});
+
+
