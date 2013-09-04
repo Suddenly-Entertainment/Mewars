1b19892e346d586b6543ecc22fc0c370ea4e9538
contact@equestrianwars.com <>
2013-09-04 22:17:16 +0000

    Forgot something

--- socket/chat_socket.js
+++ socket/chat_socket.js
@@ -5,10 +5,31 @@ var GetChats = function (socket)
   
 };
 
+var auth_users = { };
+var unauth_users = { };
+
 io.sockets.on('connection', function(socket)
 {
+  var GUID = '';
+  
+  
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  while(GUID.length() < 32)
+  {
+        GUID += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  
+  unauth_users[GUID] = socket;
+  
+  // somehow return the GUID for later use?
   
   
 });
 
+exports.AuthenticateUser = function (user)
+{
+  var 
+  
+}
 
