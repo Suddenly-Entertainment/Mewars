6748e16666eb4961eb0f832a02f9446a144d4a0f
contact@equestrianwars.com <>
2013-09-14 00:54:01 +0000

    Fixed GUID.lenght() to be just .length

--- socket/chat_socket.js
+++ socket/chat_socket.js
@@ -46,7 +46,7 @@ global.io.sockets.on('connection', function(socket)
   
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  while(GUID.length() < 32)
+  while(GUID.length < 32)
   {
         GUID += possible.charAt(Math.floor(Math.random() * possible.length));
   }
